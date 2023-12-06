884d5ce3a2fa329b756bc4ab1856d2213caa889c
TheRich904 <104098501+TheRich904@users.noreply.github.com>
2023-12-06 18:28:56 -0500

    Commit Search
    Updates on Search

---
+++ VIPS/Controllers/SearchController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VIPS.Models;
+using VIPS.Models.ViewModels.Search;
+
+namespace VIPS.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public SearchController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+
+        public IActionResult SearchView()
+        {
+            var tempcontract = _db.Contracts.Select(x => new
+            {
+                x.ContractID,
+                x.CreatedOn,
+                x.ContractName,
+                x.Owner,
+                x.StageName,
+                x.UpdatedOn,
+                x.AgencyName,
+                x.City,
+                x.Department,
+                x.FacultyInitiator,
+                x.Renewal,
+                x.State,
+                x.Year
+            });
+
+            tempcontract.ToList();
+
+            var model = new SearchViewModel()
+            {
+                ContractList = tempcontract
+            };
+
+
+            return View(model);
+
+        }
+    }
+}
---
+++ VIPS/Models/ViewModels/Search/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using VIPS.Models.Data;
+
+namespace VIPS.Models.ViewModels.Search
+{
+    public class SearchViewModel
+    {
+        public List<Contract> ContractList { get; set; }
+
+
+        public SearchViewModel()
+        {
+            ContractList = new List<Contract>();
+        }
+
+        
+    }
+}
