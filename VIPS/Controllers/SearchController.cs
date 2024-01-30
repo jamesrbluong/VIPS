4c1dbb4a538f69c7a65d8d137bea18df8a674944
TheRich904 <104098501+TheRich904@users.noreply.github.com>
2024-01-30 11:38:54 -0500

    Fixed and Small Changes

--- VIPS/Controllers/SearchController.cs
+++ VIPS/Controllers/SearchController.cs
@@ -1,98 +1,111 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Threading.Tasks;
 using VIPS.Models;
 using VIPS.Models.Data;
 using VIPS.Models.ViewModels.Search;
+using System.Collections.Generic;
 
 namespace VIPS.Controllers
 {
     public class SearchController : Controller
     {
         private readonly ApplicationDbContext _db;
+
         public SearchController(ApplicationDbContext db)
         {
             _db = db;
         }
 
-        //public async Task<IActionResult> Index(string searchString)
-        //{
-        //    ViewData["CurrentFilter"] = searchString;
-
-        //    var contracts = from c in _db.Contracts
-        //                    select c;
-
-        //    if (!String.IsNullOrEmpty(searchString))
-        //    {
-        //        contracts = contracts.Where(c => c.ContractName.Contains(searchString));
-        //    }
-
-        //    return View(await contracts.AsNoTracking().ToListAsync());
-        //}
-
-        public async Task<IActionResult> SearchView(string sortOrder, string searchString)
+        public List<Contract> SearchContractsByDepartment(string departmentName)
         {
-            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "alphabetical" : "";
-            ViewData["DateSortParm"] = sortOrder == "close_exp" ? "far_exp" : "close_exp";
-            ViewData["CurrentFilter"] = searchString;
+            var filteredContracts = _db.Contracts
+                .Where(contract => contract.FolderName.Contains(departmentName, StringComparison.OrdinalIgnoreCase))
+                .ToList();
 
-            var tempcontracts = _db.Contracts.Select(x => new CondensedContract
-            {
-                ContractID = x.ContractID,
-                CreatedOn = x.CreatedOn,
-                ContractName = x.ContractName,
-                Owner = x.Owner,
-                StageName = x.StageName,
-                UpdatedOn = x.UpdatedOn,
-                AgencyName = x.AgencyName,
-                City = x.City,
-                Department = x.Department,
-                FacultyInitiator = x.FacultyInitiator,
-                Renewal = x.Renewal,
-                State = x.State,
-                Year = x.Year
-            }).ToList();
+            return filteredContracts;
+        }
 
-            var model = new SearchViewModel
-            {
-                ContractList = tempcontracts
-            };
-
+        public async Task<IActionResult> SearchView(string searchString, string sortOrder)
+        {
+            var model = new SearchViewModel();
 
             if (!String.IsNullOrEmpty(searchString))
             {
-                model.ContractList = model.ContractList
-                    .Where(c => c.ContractName.Contains(searchString))
-                    .ToList();
+                model.ContractList = await _db.Contracts
+                    .Where(c => c.Department.ToLower() == searchString.ToLower() || c.ContractName.ToLower().Contains(searchString.ToLower()))
+                    .Select(x => new CondensedContract
+                    {
+                        ContractID = x.ContractID,
+                        CreatedOn = x.CreatedOn,
+                        ContractName = x.ContractName,
+                        Owner = x.Owner,
+                        StageName = x.StageName,
+                        UpdatedOn = x.UpdatedOn,
+                        AgencyName = x.AgencyName,
+                        City = x.City,
+                        Department = x.Department,
+                        FacultyInitiator = x.FacultyInitiator,
+                        Renewal = x.Renewal,
+                        State = x.State,
+                        Year = x.Year,
+                        // New property for ExpirationDate
+                        ExpirationDate = x.ExpirationDate
+                    }).ToListAsync();
+            }
+            else
+            {
+                model.ContractList = await _db.Contracts
+                    .Select(x => new CondensedContract
+                    {
+                        ContractID = x.ContractID,
+                        CreatedOn = x.CreatedOn,
+                        ContractName = x.ContractName,
+                        Owner = x.Owner,
+                        StageName = x.StageName,
+                        UpdatedOn = x.UpdatedOn,
+                        AgencyName = x.AgencyName,
+                        City = x.City,
+                        Department = x.Department,
+                        FacultyInitiator = x.FacultyInitiator,
+                        Renewal = x.Renewal,
+                        State = x.State,
+                        Year = x.Year,
+                        // New property for ExpirationDate
+                        ExpirationDate = x.ExpirationDate
+                    }).ToListAsync();
             }
 
-            switch (sortOrder)
+            if (sortOrder == "alphabetical")
             {
-                case "alphabetical":
-                    model.ContractList = model.ContractList.OrderByDescending(c => c.ContractName).ToList();
-                    break;
-                case "close_exp":
-                    model.ContractList = model.ContractList.OrderBy(c => DateTime.Parse(c.CreatedOn)).ToList();
-                    break;
-                case "far_exp":
-                    model.ContractList = model.ContractList.OrderByDescending(c => DateTime.Parse(c.CreatedOn)).ToList();
-                    break;
-                default:
-                    model.ContractList = model.ContractList.OrderBy(c => c.ContractName).ToList();
-                    break;
+                model.ContractList = model.ContractList.OrderBy(contract => {
+                    var contractName = contract.ContractName;
+                    var index = contractName.IndexOf("AA - ");
+                    if (index >= 0 && contractName.Length > index + 5)
+                    {
+                        var substring = contractName.Substring(index + 5);
+                        var words = substring.Split(' ');
+                        if (words.Length > 0)
+                        {
+                            return words[0];
+                        }
+                    }
+                    return contractName;
+                }).ToList();
             }
+            else if (sortOrder == "close_exp")
+            {
+                model.ContractList = model.ContractList.OrderBy(contract => contract.ExpirationDate).ToList();
+            }
+            else if (sortOrder == "far_exp")
+            {
+                model.ContractList = model.ContractList.OrderByDescending(contract => contract.ExpirationDate).ToList();
+            }
+            // Add other sorting logics for 'id' if needed...
 
             return View(model);
-
         }
-
     }
-
 }
-
-
-
-   
